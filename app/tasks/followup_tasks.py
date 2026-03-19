db189fd80fa2bcc1f5842e559cffbca9f0ad7899
jcfroesjr <jcfroesjr@gmail.com>
2026-03-18 23:35:58 -0300

    Add Celery task for verifying pending follow-ups

--- app/tasks/followup_tasks.py
+++ app/tasks/followup_tasks.py
@@ -1,1 +1,9 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
 
+logger = get_task_logger(__name__)
+
+@shared_task
+def verificar_followups_pendentes():
+    logger.info("Verificando follow-ups pendentes...")
+    return {"ok": True}
