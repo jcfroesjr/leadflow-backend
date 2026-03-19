52d94773240d3344eaf3e692a7f2863a49fdf400
jcfroesjr <jcfroesjr@gmail.com>
2026-03-18 23:36:57 -0300

    Add Celery task to clean old appointments

--- app/tasks/agendamento_tasks.py
+++ app/tasks/agendamento_tasks.py
@@ -1,1 +1,9 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
 
+logger = get_task_logger(__name__)
+
+@shared_task
+def limpar_agendamentos_antigos():
+    logger.info("Limpando agendamentos antigos...")
+    return {"ok": True}
